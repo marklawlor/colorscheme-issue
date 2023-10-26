770cb81f376aee16b4c71efbf89142ce464d9cd4
Mark Lawlor <mwlawlor@gmail.com>
2023-10-26 11:18:30 +1000

    issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { Appearance, StyleSheet, useColorScheme } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Text, View } from "../../components/Themed";
+import { useState } from "react";
 
 export default function TabOneScreen() {
+  const colorScheme = useColorScheme();
+  const [clicks, setClick] = useState(0);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text
+        style={styles.title}
+        onPress={() => {
+          setClick((clicks) => clicks + 1);
+          Appearance.setColorScheme(colorScheme === "light" ? "dark" : "light");
+        }}
+      >
+        Current color scheme is {colorScheme} and you have pressed {clicks}.
+        Press to toggle.
+      </Text>
     </View>
   );
 }
@@ -16,16 +25,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
